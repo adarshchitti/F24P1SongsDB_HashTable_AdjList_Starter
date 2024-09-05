2190160cbdc282c3261d39955d88ba6631a4fb3f
branboy <brantson.q.bui@gmail.com>
2024-09-05 16:36:22 -0400

    comments

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,12 @@
-
+/**
+ * The Controller class manages a graph structure representing artists and
+ * songs.
+ * It supports inserting artists and songs, printing the graph, and removing
+ * artists or songs.
+ * 
+ * @author Brantson and Adarsh
+ * @version 09.05.2024
+ */
 public class Controller {
     public Hash songs;
     public Hash artists;
@@ -8,6 +16,12 @@ public class Controller {
     private int dst;
     private int length;
 
+    /**
+     * Constructs a Controller with the given length for hash tables and graph.
+     *
+     * @param length
+     *            the initial size of the hash tables and the graph
+     */
     public Controller(int length) {
         songs = new Hash(length);
         artists = new Hash(length);
@@ -16,6 +30,17 @@ public class Controller {
     }
 
 
+    /**
+     * Inserts an artist and song into the database and updates the graph.
+     * If the artist or song is already present, the method will handle
+     * duplicates.
+     * Also checks and doubles the size of the hash table if necessary.
+     *
+     * @param artist
+     *            the name of the artist to insert
+     * @param song
+     *            the name of the song to insert
+     */
     public void insert(String artist, String song) {
         if (artists.find(artist) == -1) {
             src = index;
@@ -63,34 +88,46 @@ public class Controller {
     }
 
 
+    /**
+     * Prints the current state of the graph, including the number of connected
+     * components
+     * and the size of the largest connected component.
+     */
     public void printGraph() {
-
         System.out.println("There are " + fullGraph.connectedComponents()
             + " connected components");
         System.out.println("The largest connected component has " + fullGraph
             .connectedElements() + " elements");
-        /*
-         * fullGraph.print();
-         * System.out.println(fullGraph.connectedElements());
-         * System.out.println(fullGraph.connectedComponents());
-         */
     }
 
 
+    /**
+     * Prints the list of all artists in the database and the total number of
+     * artists.
+     */
     public void printArtist() {
         System.out.print(artists.print());
         System.out.println("total artists: " + artists.getNumberOfRecords());
-
     }
 
 
+    /**
+     * Prints the list of all songs in the database and the total number of
+     * songs.
+     */
     public void printSong() {
         System.out.print(songs.print());
         System.out.println("total songs: " + songs.getNumberOfRecords());
-
     }
 
 
+    /**
+     * Removes an artist from the database and the graph.
+     * If the artist does not exist, an appropriate message is printed.
+     *
+     * @param value
+     *            the name of the artist to remove
+     */
     public void removeArtist(String value) {
         if (artists.find(value) != -1) {
             fullGraph.removeRecord(value);
@@ -102,10 +139,16 @@ public class Controller {
             System.out.println("|" + value
                 + "| does not exist in the Artist database.");
         }
-
     }
 
 
+    /**
+     * Removes a song from the database and the graph.
+     * If the song does not exist, an appropriate message is printed.
+     *
+     * @param value
+     *            the name of the song to remove
+     */
     public void removeSong(String value) {
         if (songs.find(value) != -1) {
             fullGraph.removeRecord(value);
@@ -117,6 +160,5 @@ public class Controller {
             System.out.println("|" + value
                 + "| does not exist in the Song database.");
         }
-
     }
 }
